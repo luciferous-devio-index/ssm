39c84c58e53e7b559e4e307ad2d84b1097565647
sinofseven <em.s.00001@gmail.com>
2023-04-18 16:16:03 +0900

    impl

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "remote" {
+    organization = "luciferous-devio-index"
+    workspaces {
+      name = "ssm"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=4.63.0"
+    }
+  }
+  required_version = "=1.4.5"
+}
+
+variable "OPEN_AI_API_KEY" {}
+variable "SLACK_INCOMING_WEBHOOK" {}
+
+resource "aws_kms_key" "encrypt" {}
+
+resource "aws_ssm_parameter" "open_ai_api_key" {
+  name   = "/LuciferousDevIoIndex/Secrets/OpenAiApiKey"
+  type   = "SecureString"
+  key_id = aws_kms_key.encrypt.arn
+  value  = var.OPEN_AI_API_KEY
+}
+
+resource "aws_ssm_parameter" "slack_incoming_webhook" {
+  name   = "/LuciferousDevIoIndex/Secrets/SlackIncomingWebhook"
+  type   = "SecureString"
+  key_id = aws_kms_key.encrypt.arn
+  value  = var.SLACK_INCOMING_WEBHOOK
+}
